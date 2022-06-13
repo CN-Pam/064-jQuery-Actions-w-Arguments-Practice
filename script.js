b33961483e28cf3b7d909414b0a81b5adeffc6a7
pamela-martinez <72035155+pamela-martinez@users.noreply.github.com>
2022-06-13 09:56:11 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+// Guided Practice
+$(".reply-yes").click(function() {  
+    $(".homer-reply").text("Woo Hoo!");
+    $(".reply-yes").css("background-color", "yellow");
+});
+
+//Independent Practice (START YOUR CODE BELOW!)
+$(".reply-no").click(function() {  
+     $(".homer-reply").text("Doh!");
+        $("h1").css("color", "red");
+    
+});
+
